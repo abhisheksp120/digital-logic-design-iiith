1f74de552c6316cdebc3df96456581cacf717e0d
Abhishek <abhipadghan00@gmail.com>
2019-06-25 18:59:11 +0530

    Fixed resizing

--- SRIP/Codes/Js.js
+++ SRIP/Codes/Js.js
@@ -1,11 +1,52 @@
+  var x=$("#div").width();
+  var y=x*1.2;
+
+var counter={
+    "width":y,
+            "height":800,
+            "showToolbox":true,
+            "toolbox":[
+            {"type":"In"},
+            {"type":"Out"},
+            {"type":"DC"},
+            {"type":"OSC","freq":1},
+            {"type":"Joint"},
+            {"type":"LED"},
+            {"type":"PushOn"},
+            {"type":"PushOff"},
+            {"type":"Toggle"},
+            {"type":"NOT"},       
+            {"type":"AND"},
+            {"type":"NAND"},
+            {"type":"OR"},
+            {"type":"4bitAdder"},
+            {"type":"Parallel Shift Reg"},
+            {"type":"DSO","numInputs":6},
+            {"type":"Selector"},
+            {"type":"JK-FF"},
+            {"type":"D-FF"},
+            {"type":"FullAdder"}
+    ],
+    "devices":[
+   
+    ],
+    "connectors":[
+   
+    ]
+  };
+var html1=JSON.stringify(counter);
+  document.querySelector(".simcir").innerHTML=html1;
+  
+$(".simcir_outer_div").width(x*0.85);
+
    function change1(user){
    $(document).ready(function(){ 
                 $(".button3").click(function(){ 
                 if (user==="1") {
-                	 document.getElementById("div4").innerHTML='<object type="text/html" data="Circuits/Parallel Shift Reg.html" width="2400" height="1000" left="100"></object>';
+                	 document.getElementById("div4").innerHTML='<object type="text/html" data="Circuits/Parallel Shift Reg.html" width="2800" height="1000" left="100"></object>';
                 }
                 else{
-                	document.getElementById("div4").innerHTML='<object type="text/html" data="Circuits/FullAdder.html" width="2400" height="1000" left="100"></object>';
+                	document.getElementById("div4").innerHTML='<object type="text/html" data="Circuits/FullAdder.html"  width="2800" height="1000" left="100" ></object>';
                 }
                 });
             }); 
